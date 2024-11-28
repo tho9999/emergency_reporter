3cfee20dc0e9b2af3037f8cc3c3fc244e0df1d6a
Tyler Ho <tiho@sfu.ca>
2024-11-27 22:34:51 -0800

    Fixed photo display for figure

--- src/mainPage/mainPageComponents/figure.jsx
+++ src/mainPage/mainPageComponents/figure.jsx
@@ -1,9 +1,13 @@
 import "../mainPage.css";
 
 function Figure({ incident }) {
+    
+    // If there is a picture then create URL to picture if not pictureSource is null
+    const pictureSource = incident.getPicture() ? URL.createObjectURL(incident.getPicture()) : null;
+
     return (
         <figure>
-            {incident.getPicture() && <img src={`${incident.getPicture()}`} />}
+            {incident.getPicture() && <img src={pictureSource} alt="Incident" />}
             <p><strong>Type: </strong>{incident.getEmergencyInfo()}</p>
             <p><strong>Reported by: </strong>{incident.getWitnessName()}  ({incident.getWitnessPhone()})</p>
             <p><strong>Time: </strong>{incident.getTime()} </p>
